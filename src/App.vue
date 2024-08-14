baa58ded429327d12eac0832677116ef63c2f237
RubyVine01 <leraprokofevaiera1@gmail.com>
2024-08-14 14:20:11 +0300

    add main layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
-<script setup>
-
-</script>
-
 <template>
   <h1>Crypto</h1>
+  <Input />
+  <div className="selectors">
+    <Selector />
+    <Selector />
+  </div>
 
 </template>
 
-<style scoped></style>
+<script>
+import Input from './components/Input.vue';
+import Selector from './components/Selector.vue'
+export default {
+  components: { Input, Selector }
+}
+</script>
+
+
+<style scoped>
+.selectors {
+  display: flex;
+  justify-content: space-around;
+  width: 700px;
+  margin: 20px auto;
+}
+</style>
---
+++ src/components/Input.vue
@@ -0,0 +1,23 @@
+<template>
+    <input type="number">
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style scoped>
+input {
+    outline: none;
+    border-radius: 3px;
+    border: 0;
+    padding: 10px 15px;
+    background: #fafafa;
+    color: #333;
+    width: 500px;
+    font-size: 1.2em;
+}
+</style>
